fdac40b4bee062f96e11aa97260b1a7c5ea9c52a
于海翔 <yoohaythem@gmail.com>
2023-10-15 14:45:33 +0800

    2023.10.15 灯光、火光效果、人物移动

---
+++ 3. 古迹探险-2023.10/Assets/Scripts/Hero.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Hero : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    public Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if(Physics.Raycast(ray, out hit))
+            {
+                agent.SetDestination(hit.point);
+            }
+        }
+
+        anim.SetFloat("Speed", agent.velocity.magnitude);
+    }
+}
